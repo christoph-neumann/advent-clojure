77b88f11f88dc268d3d016c99c01a45c10811d1c
Christoph Neumann <christoph@neumannhaus.com>
2015-12-12 11:53:03 -0500

    Day 01, Second Puzzle

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -8,6 +8,16 @@
   [input]
   (str "floor " (reduce floorinc 0 input)))
 
+(defn day01b
+  "Day 01, Second Puzzle"
+  [input]
+  (->> input
+    (reductions floorinc 0)
+    (map-indexed vector)
+    (filter #(= -1 (nth %1 1)))
+    (first)
+    (first)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -13,3 +13,10 @@
     (is (= "floor -1" (day01 "))(")))
     (is (= "floor -3" (day01 ")))")))
     (is (= "floor -3" (day01 ")())())")))))
+
+(deftest day01b-test
+  (testing "Day 01, Second Puzzle"
+    (is (= 3 (day01b "())")))
+    (is (= 1 (day01b "))(")))
+    (is (= 1 (day01b ")))")))
+    (is (= 5 (day01b "(()))")))))
